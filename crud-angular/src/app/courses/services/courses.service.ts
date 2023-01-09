148fa51e15cc05dc1ce9167a3fd2a355cf4f8e30
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-01-09 09:37:22 -0300

    feat: pipe category icons

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Course } from '../model/course';
-import { delay, first, tap } from 'rxjs';
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,11 +12,9 @@ export class CoursesService {
   constructor(private httpClient: HttpClient) {}
 
   getCourses() {
-    return this.httpClient
-      .get<Course[]>(this.API)
-      .pipe(
-        first(),
-        delay(5000),
-        tap((courses) => console.log(courses)));
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      tap((courses) => console.log(courses))
+    );
   }
 }

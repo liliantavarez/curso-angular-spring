b93ca00657683bc4dcb937e49c013f54f7efd870
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-01-12 10:18:41 -0300

    chore: configurando proxy

---
+++ crud-angular/proxy.config.js
@@ -0,0 +1,11 @@
+// Configurado proxy
+const PROXY_CONFIG = [
+  {
+    context: ["/api"],
+    target: "http://localhost:8080",
+    secure: false,
+    logLevel: "debug",
+  },
+];
+
+module.exports = PROXY_CONFIG;

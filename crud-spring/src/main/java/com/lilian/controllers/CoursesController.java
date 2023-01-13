c71aa3c0df11a85397fd541b844e5b672352de22
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-01-13 09:39:59 -0300

    feat: controller create course

--- crud-spring/src/main/java/com/lilian/controllers/CoursesController.java
+++ crud-spring/src/main/java/com/lilian/controllers/CoursesController.java
@@ -2,9 +2,15 @@ package com.lilian.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lilian.model.Course;
@@ -14,7 +20,7 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/courses")
-@AllArgsConstructor //Cria o construtor
+@AllArgsConstructor // Cria o construtor
 
 @Component
 public class CoursesController {
@@ -22,7 +28,13 @@ public class CoursesController {
     private final CourseRepository courseRepository;
 
     @GetMapping
-    public List<Course> list() {
+    public List<Course> getAll() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course post(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 }
